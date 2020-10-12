8240efc3df98540b6141ad69bcd43f068668ffab
blackbird014 <andrehmerli@gmail.com>
2020-10-12 20:47:28 +0200

    fixed error in transferring to seller the full amount instead of the price.

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -116,10 +116,10 @@ contract SupplyChain {
     refunded any excess ether sent. Remember to call the event associated with this function!*/
 
   function buyItem(uint sku)
-    public payable forSale(sku)/*forSale(sku) paidEnough(items[sku].price) checkValue(sku)*/
+    public payable forSale(sku) paidEnough(items[sku].price) checkValue(sku)
   {
+    items[sku].seller.transfer(items[sku].price);
     items[sku].buyer = msg.sender;
-    items[sku].seller.transfer(msg.value);
     items[sku].state = State.Sold;
     emit LogSold(sku);
   }
